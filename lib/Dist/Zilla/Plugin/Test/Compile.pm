e10ece4f2ad9e0578a407414374881ca1538edf5
Karen Etheridge <ether@cpan.org>
2013-07-14 11:39:31 -0700

    resolve mswin32 failures
    cmd.exe doesn't use single quotes, so we were getting something like:
    perl -Ilib -e "require module; print "module ok""
    
    e.g. see
    http://www.cpantesters.org/cpan/report/4b3f80ef-6e3d-1014-b109-8d9eb77d427c
    
    There may be more issues, but we need to solve this one first before we can
    clearly see them.

--- lib/Dist/Zilla/Plugin/Test/Compile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile.pm
@@ -250,7 +250,7 @@ do { push @scripts, _find_scripts($_) if -d $_ }
     for my $module (sort @modules)
     {
         my ($stdout, $stderr, $exit) = capture {
-            system($^X, '-Ilib', '-e', qq{require $module; print "$module ok"});
+            system($^X, '-Ilib', '-e', qq{require $module; print qq[$module ok]});
         };
         like($stdout, qr/^\s*$module ok/s, "$module loaded ok" );
         warn $stderr if $stderr;
