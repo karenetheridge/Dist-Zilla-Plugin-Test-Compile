c0df841839bbc6fad9368e6ed04913dfbbd75c13
Karen Etheridge <ether@cpan.org>
2013-07-29 17:46:07 -0700

    go back to testing .pm files with -Ilib

--- lib/Dist/Zilla/Plugin/Test/Compile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile.pm
@@ -302,7 +302,7 @@ my @warnings;
 for my $lib (@module_files)
 {
     my ($stdout, $stderr, $exit) = capture {
-        system($^X, '-Mblib', '-e', qq{require qq[$lib]});
+        system($^X, '-Ilib', '-e', qq{require qq[$lib]});
     };
     is($?, 0, "$lib loaded ok");
     warn $stderr if $stderr;
