7f8f063cd1ea9bbc33d990210c173f9c35686770
Karen Etheridge <ether@cpan.org>
2013-08-11 16:04:54 -0700

    depend on a fixed Data::Section

--- lib/Dist/Zilla/Plugin/Test/Compile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile.pm
@@ -8,7 +8,9 @@ package Dist::Zilla::Plugin::Test::Compile;
 use Moose;
 use Path::Tiny;
 use Sub::Exporter::ForMethods 'method_installer'; # method_installer returns a sub.
-use Data::Section { installer => method_installer }, -setup;
+use Data::Section 0.004 # fixed header_re
+    { installer => method_installer }, '-setup';
+
 with (
     'Dist::Zilla::Role::FileGatherer',
     'Dist::Zilla::Role::FileMunger',
