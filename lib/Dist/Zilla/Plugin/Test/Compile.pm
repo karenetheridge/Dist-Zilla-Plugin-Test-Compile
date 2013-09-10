4d97d98337633109f57145e1f0556b857ede92c8
Karen Etheridge <ether@cpan.org>
2013-09-09 18:51:00 -0700

    spelling

--- lib/Dist/Zilla/Plugin/Test/Compile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile.pm
@@ -245,7 +245,7 @@ This is the name of a L<FileFinder|Dist::Zilla::Role::FileFinder> for finding
 modules to check.  The default value is C<:InstallModules>; this option can be
 used more than once.  .pod files are always omitted.
 
-Other pre-defined finders are listed in
+Other predefined finders are listed in
 L<Dist::Zilla::Role::FileFinderUser/default_finders>.
 You can define your own with the
 L<[FileFinder::ByName]|Dist::Zilla::Plugin::FileFinder::ByName> plugin.
