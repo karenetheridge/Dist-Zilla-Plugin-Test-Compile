9e98c413c7e2907f76cb3e308eaf7d3b9f3c0561
Karen Etheridge <ether@cpan.org>
2013-09-21 10:44:43 -0700

    test all of $?, for things like core dumps (thanks ribasushi)

--- lib/Dist/Zilla/Plugin/Test/Compile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile.pm
@@ -340,7 +340,7 @@ for my $lib (@module_files)
     binmode $stderr, ':crlf' if $^O eq 'MSWin32';
     my @_warnings = <$stderr>;
     waitpid($pid, 0);
-    is($? >> 8, 0, "$lib loaded ok");
+    is($?, 0, "$lib loaded ok");
 
     if (@_warnings)
     {
@@ -367,7 +367,7 @@ foreach my $file (@scripts)
     binmode $stderr, ':crlf' if $^O eq 'MSWin32';
     my @_warnings = <$stderr>;
     waitpid($pid, 0);
-    is($? >> 8, 0, "$file compiled ok");
+    is($?, 0, "$file compiled ok");
 
    # in older perls, -c output is simply the file portion of the path being tested
     if (@_warnings = grep { !/\bsyntax OK$/ }
